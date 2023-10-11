d321cd44c3b8e71eb152d90b2a9377ca9c9b24db
qbzenker <johnzenk11@gmail.com>
2023-10-11 06:35:10 -0600

    typing misc

--- ecoscope/analysis/geofence.py
+++ ecoscope/analysis/geofence.py
@@ -10,7 +10,7 @@ import ecoscope
 
 
 class Region(collections.UserDict):
-    def __init__(self, geometry: typing.Any, unique_id: str = None, region_name: str = None):
+    def __init__(self, geometry: typing.Any, unique_id: str | None = None, region_name: str | None = None):
         super().__init__(
             geometry=geometry,
             unique_id=unique_id,
@@ -24,9 +24,9 @@ class GeoFence(collections.UserDict):
     def __init__(
         self,
         geometry: typing.Any,
-        unique_id: str = None,
-        fence_name: str = None,
-        warn_level: str = None,
+        unique_id: str | None = None,
+        fence_name: str | None = None,
+        warn_level: str | None = None,
     ):
         super().__init__(
             geometry=geometry,
@@ -66,7 +66,7 @@ class GeoFenceCrossing:
         geocrossing_profile: GeoCrossingProfile
             Object that contains the geonfences and regions
         trajectory: ecoscope.base.Trajectory
-            Geodataframe stores goemetry, speed_kmhr, heading etc. for each subject.
+            Geodataframe stores geometry, speed_kmhr, heading etc. for each subject.
 
         Returns
         -------
